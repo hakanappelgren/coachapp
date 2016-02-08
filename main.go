993b45e4ce0d1aae64ee509170c008f7cc6af826
hakanappelgren <hakan.j.appelgren@gmail.com>
2016-02-08 12:33:04 +0100

    Initial Commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import
+  "net/http"
+  "os"
+
+
+func main() {
+  port := os.Getenv("PORT")
+    if port == "" {
+  port = "8080"
+  }
+  // handle the specific requests
+  http.HandleFunc("/coachquestion", ShowQuestsion)
+  //  http.HandleFunc("/coachquestion", GenerateMarkdown)
+  // handle all other requests
+  http.Handle("/", http.FileServer(http.Dir("public")))
+  // start the server
+  http.ListenAndServe(":"+port, nil)
+  // http.ListenAndServe(":8080", nil)
+
+
+  // http.ListenAndServe(":8080", http.xxx(http.Dir(".")))
+
+  func ShowQuestsion(rw http.ResponseWriter, r *http.Request) {
+    thecoachquestion := "första frågan"
+//    markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
+    rw.Write(thecoachquestion)
+}
+}
