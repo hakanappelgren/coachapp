b286baf25aa9f78a93665fb1d45c05278e25fa39
hakanappelgren <hakan.j.appelgren@gmail.com>
2016-02-10 09:18:51 +0100

    Test update of index page

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import(
   "net/http"
   "os"
+  //"math.rand"
 )
 
 func main() {
@@ -11,19 +12,21 @@ func main() {
   port = "8080"
   }
   // handle the specific requests
-  http.HandleFunc("/coachquestion", ShowQuestsion)
+  http.HandleFunc("/coachquestion", ShowQuestion)
   //  http.HandleFunc("/coachquestion", GenerateMarkdown)
   // handle all other requests
+  //ToDo: have to change this action and serve the start page insted och the FileServer stuff
   http.Handle("/", http.FileServer(http.Dir("public")))
   // start the server
   http.ListenAndServe(":"+port, nil)
   // http.ListenAndServe(":8080", nil)
   // http.ListenAndServe(":8080", http.xxx(http.Dir(".")))
 }
-  func ShowQuestsion(rw http.ResponseWriter, r *http.Request) {
+  func ShowQuestion(rw http.ResponseWriter, r *http.Request) {
 //    questionlist := []byte {'f', 'a'}
 //    questionlist := []byte ("min första fråga")
     questionlist := [...]string{"fråga 1", "fråga 2"}
+    // index = rand.int31n(2)
     thequestion := questionlist[1]
 //    questionlist := "min första fråga"
 
