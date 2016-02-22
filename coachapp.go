7b8c35c940fc2c9ab7db302162a85555a7bd0492
hakanappelgren <hakan.j.appelgren@gmail.com>
2016-02-22 13:28:21 +0100

    Update sequence of questions

--- coachapp.go
+++ coachapp.go
@@ -5,6 +5,7 @@ import(
   "html/template"
   "os"
   "path"
+  "fmt"
   //"math.rand"
 )
 
@@ -16,11 +17,12 @@ type Question struct {
 }
 
 func main() {
-
+  fmt.Printf("början av main.\n")
   port := os.Getenv("PORT")
     if port == "" {
   port = "8080"
   }
+  
   // handle the specific requests
   http.HandleFunc("/coachquestion", ShowQuestion)
   //  http.HandleFunc("/coachquestion", GenerateMarkdown)
@@ -32,18 +34,16 @@ func main() {
   // http.ListenAndServe(":8080", nil)
   // http.ListenAndServe(":8080", http.xxx(http.Dir(".")))
 }
+
   func ShowAbout (rw http.ResponseWriter, r *http.Request) {
 
 }
+
   func ShowQuestion(rw http.ResponseWriter, r *http.Request) {
     questionlist := [...]string{"fråga 1", "fråga 2"}
     // index = rand.int31n(2)
-    if qindex < 1 {
-      qindex = qindex + 1
-    } else {
-      qindex = 0
-    }
-    myquestion := Question{questionlist[qindex]}
+    myquestion := Question{questionlist[GetIndex]}
+//        rw.Write([]byte("fråga 1"))
 
     fp := path.Join("public", "question.html")
     tmpl, err := template.ParseFiles(fp)
@@ -55,5 +55,15 @@ func main() {
         http.Error(rw, err.Error(), http.StatusInternalServerError)
     }
 
-//    rw.Write([]byte(thequestion))
+}
+
+  func GetIndex() int {
+    if qindex < 1 {
+      fmt.Printf("qindex < 1, dvs =0 \n")
+      qindex = qindex + 1
+    } else {
+      fmt.Printf("qindex > 0, dvs =1 \n")
+      qindex = 0
+    }
+   return qindex
 }
