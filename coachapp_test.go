e70e9df8269e41a2fd0b70fc2dbc8a0b1b9a99a3
hakanappelgren <hakan.j.appelgren@gmail.com>
2016-07-02 03:55:17 -0400

    unit test

--- coachapp_test.go
+++ coachapp_test.go
@@ -1,23 +1,23 @@
-package testutil
+package main
 
 import "testing"
 
 
 func TestGetIndex (t *testing.T) {
 	cases := []struct {
-		israndom boolean,
-		length int,
-		currentindex int,
+		israndom bool;
+		length int;
+		currentindex int;
 		want int
 	}{
 		{false, 3, 0, 1},
 		{false, 3, 1, 2},
-		{false, 3, 2, 0}
+		{false, 3, 2, 0},
 	}
 		for _, c := range cases {
 			got := GetIndex(c.length, c.israndom, c.currentindex)
 			if got != c.want {
-				t.Errorf("Reverse(%q) == %q, want %q", c.in, got, c.want)
+				t.Errorf("Reverse(%q) == %q, want %q", c.israndom, c.length, c.currentindex, c.want)
 			}
 		}
 }
